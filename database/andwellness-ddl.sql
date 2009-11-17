62bcc2a76f5027fcb7efddd515eae12e177f4ba3
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2009-11-17 00:53:26 +0000

    Latest updates after team review and further refinement

--- database/andwellness-ddl.sql
+++ database/andwellness-ddl.sql
@@ -1,5 +1,7 @@
 -- MySQL DDL statements for the AndWellness database
 -- Version 0.9.0
+-- As the number of tables in the database grows, it would be a good idea 
+-- to split this file up separate files per table.
 
 CREATE DATABASE andwellness CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE andwellness;
@@ -32,12 +34,11 @@ CREATE TABLE campaign (
 -- ---------------------------------------------------------------------
 CREATE TABLE user (
   id smallint(6) unsigned NOT NULL auto_increment,
-  email_address varchar(320) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL, -- cannot use UTF-8 here because the field will
-                                                                               -- exceed the max number of characters that MySQL
-                                                                               -- will index.
+  email_address varchar(320)
   json_data text,
   PRIMARY KEY (id),
-  UNIQUE (email_address)
+  -- we will have to check the uniqueness of new email addresses within the application itself because 
+  -- the length of the UTF-8 encoded email address exceeds the maximum size for indexing in MySQL.
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------------------
@@ -80,9 +81,12 @@ CREATE TABLE prompt_response (
   prompt_id smallint unsigned NOT NULL,
   user_id smallint(6) unsigned NOT NULL,
   campaign_id smallint(4) unsigned NOT NULL,
-  time_stamp timestamp NOT NULL,
-  epoch_millis bigint unsigned NOT NULL, 
-  json_data text, 
+  utc_time_stamp timestamp NOT NULL,
+  utc_epoch_millis bigint unsigned NOT NULL, 
+  phone_timezone varchar (32) NOT NULL,
+  latitude double NOT NULL,
+  longitude double NOT NULL,
+  json_data text NOT NULL, 
   PRIMARY KEY (id),
   CONSTRAINT FOREIGN KEY (prompt_id) REFERENCES prompt (id),
   CONSTRAINT FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -108,10 +112,11 @@ CREATE TABLE prompt_campaign (
 CREATE TABLE mobility_mode_only_entry (
   id bigint unsigned NOT NULL auto_increment,
   user_id smallint(6) unsigned NOT NULL,
-  time_stamp timestamp NOT NULL,
-  epoch_millis bigint unsigned NOT NULL,
-  latitude decimal(14,12) NOT NULL,
-  longitude decimal(15,12) NOT NULL,
+  utc_time_stamp timestamp NOT NULL,
+  utc_epoch_millis bigint unsigned NOT NULL,
+  phone_timezone varchar(32) NOT NULL,
+  latitude double NOT NULL,
+  longitude double NOT NULL,
   mode varchar(30) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE idx_epoch_user (user_id, epoch_millis),
@@ -125,52 +130,50 @@ CREATE TABLE mobility_mode_only_entry (
 CREATE TABLE mobility_mode_features_entry (
   id bigint unsigned NOT NULL auto_increment,
   user_id smallint(6) unsigned NOT NULL,
-  time_stamp timestamp NOT NULL,
-  timezone varchar(9) NOT NULL,
-  epoch_millis bigint unsigned NOT NULL,
-  latitude decimal(14,12) NOT NULL,
-  longitude decimal(15,12) NOT NULL,
+  utc_time_stamp timestamp NOT NULL,
+  utc_epoch_millis bigint unsigned NOT NULL,
+  phone_timezone varchar(32) NOT NULL,
+  latitude double NOT NULL,
+  longitude double NOT NULL,
   mode varchar(30) NOT NULL,
-  
-  -- TBC precision for these columns
-  -- speed decimal(5,3) 
-  -- variance decimal(5,3) 
-  -- average decimal(5,3)
-  -- fft varchar(100)
-  
+  speed double NOT NULL,
+  variance double NOT NULL,
+  average double NOT NULL,
+  fft varchar(300) NOT NULL, -- A comma separated list of 10 FFT floating-point values. The reason the array is not unpacked  
+                             -- into separate columns is because the data will not be used outside of a debugging scenario.
+                             -- It is simply stored the way it is sent by the phone (minus the JSON array brackets). 
   PRIMARY KEY (id),
   UNIQUE INDEX idx_epoch_user (user_id, epoch_millis),
   CONSTRAINT FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ---------------------------------------------------------------------------------
--- 5 minute summary of mobility data from mobility_mode_only_entry_raw and
--- mobility_mode_feature_entry_raw
+-- 5 minute summary of mobility data from mobility_mode_only_entry and
+-- mobility_mode_features_entry
 -- ---------------------------------------------------------------------------------
 CREATE TABLE mobility_entry_five_min_summary (
   id bigint unsigned NOT NULL auto_increment,
   user_id smallint(6) unsigned NOT NULL,
-  time_stamp timestamp NOT NULL,
-  timezone varchar(9) NOT NULL,
+  utc_time_stamp timestamp NOT NULL,
+  phone_timezone varchar(32) NOT NULL,
   mode varchar(30) NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ---------------------------------------------------------------------------------
--- Daily summary of mobility data from mobility_mode_only_entry_raw and
--- mobility_mode_feature_entry_raw
+-- Daily summary of mobility data from mobility_mode_only_entry and
+-- mobility_mode_features_entry
 -- ---------------------------------------------------------------------------------
 CREATE TABLE mobility_entry_daily_summary (
   id bigint unsigned NOT NULL auto_increment,
   user_id smallint(6) unsigned NOT NULL,
   entry_date date NOT NULL,
-  timezone varchar(9) NOT NULL,
   mode varchar(30) NOT NULL,
+  duration smallint (5) unsigned NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- TODO
--- SQL to initialize 
--- Split this file apart into separate components for each table.
+-- SQL to initialize
