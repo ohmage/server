0191eea33c6a3381ff75a832f1fddfcd68dbbbb5
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-06 01:15:45 +0000

    initial commit of command-line registration feature

---
+++ src/edu/ucla/cens/awuser/AddUserToDb.java
@@ -0,0 +1,18 @@
+package edu.ucla.cens.awuser;
+
+/**
+ * Utility class to add a new user to the AW database. This is our lazy registration feature.
+ * 
+ * @author selsky
+ */
+public class AddUserToDb {
+		
+	/**
+	 *  
+	 * 
+	 */
+	public static void main(String args[]) {
+		
+	}
+	
+}
---
+++ src/edu/ucla/cens/awuser/CreateUsername.java
@@ -0,0 +1,56 @@
+package edu.ucla.cens.awuser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Utility class to create user names. 
+ * 
+ * @author selsky
+ */
+public class CreateUsername {
+	
+	/**
+	 * Given a file name containing a newline-separated list of words, return a dot-separated concatenation of two randomly chosen
+	 * words from the file. 
+	 */
+	public static void main(String args[]) throws IOException {
+		
+		if(args.length < 1) {
+			System.err.println("a filename of a file containing the wordlist is required");
+		}
+		
+		String fileName = args[0];
+		BufferedReader in = new BufferedReader(new FileReader(fileName)); // this will throw an IOException if the file does not exist
+		List<String> words = new ArrayList<String>();
+		
+		String word = in.readLine();
+		
+		if(null == word) {
+			throw new IllegalArgumentException("there are no words in the file");
+		}
+		
+		while(null != word) { // Lazily read the whole thing into memory
+                    	 	  // The default file contains 9189 words
+			
+			words.add(word);
+			word = in.readLine();
+			
+		}
+		
+		in.close();
+		
+		int size = words.size();
+		Random random = new Random();
+		
+		int indexA = Math.abs(random.nextInt() % size);
+		int indexB = Math.abs(random.nextInt() % size);
+		
+		System.out.println("The new user name is: " + words.get(indexA) + "." + words.get(indexB));
+		
+	}	
+}
