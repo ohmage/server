ee7ca571f8f230a7aca542a6faa75153b63eb11d
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-14 18:34:08 +0000

    formerly known as AnnotatingValidator

---
+++ src/edu/ucla/cens/awserver/validator/AbstractAnnotatingValidator.java
@@ -0,0 +1,25 @@
+package edu.ucla.cens.awserver.validator;
+
+/**
+ * Abstract base class for Validators which need to annotate an AwRequest as part of their processing.
+ * 
+ * @author selsky
+ */
+public abstract class AbstractAnnotatingValidator implements Validator {
+	private AwRequestAnnotator _annotator;
+	
+	/**
+	 * @throws IllegalArgumentException if the provided AnnotateAwRequestStrategy is null
+	 */
+	public AbstractAnnotatingValidator(AwRequestAnnotator annotator) {
+		if(null == annotator) {
+			throw new IllegalArgumentException("an AwRequestAnnotationStrategy is required");
+		}
+		
+		_annotator = annotator;
+	}
+	
+	protected AwRequestAnnotator getAnnotator() {
+		return _annotator;
+	}
+}
