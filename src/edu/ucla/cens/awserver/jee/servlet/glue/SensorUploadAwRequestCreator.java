8227a2cc2157458d7c1fc99f47bebf0456e4874c
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-14 01:24:52 +0000

    converts http request to AWRequest for sensor data upload -- initial version

---
+++ src/edu/ucla/cens/awserver/jee/servlet/glue/SensorUploadAwRequestCreator.java
@@ -0,0 +1,53 @@
+package edu.ucla.cens.awserver.jee.servlet.glue;
+
+import javax.servlet.http.HttpServletRequest;
+
+import edu.ucla.cens.awserver.datatransfer.AwRequest;
+import edu.ucla.cens.awserver.datatransfer.AwRequestImpl;
+import edu.ucla.cens.awserver.domain.UserImpl;
+import edu.ucla.cens.awserver.util.StringUtils;
+
+/**
+ * Transformer for creating an AwRequest for authentication.
+ * 
+ * @author selsky
+ */
+public class SensorUploadAwRequestCreator implements AwRequestCreator {
+//	private static Logger _logger = Logger.getLogger(SensorUploadAwRequestCreator.class);
+	
+	/**
+	 * Default no-arg constructor. Simply creates an instance of this class.
+	 */
+	public SensorUploadAwRequestCreator() {
+		
+	}
+	
+	/**
+	 *  Pulls the u (userName), t (type), phv (phone version), prv (protocol version), and d (json data) parameters out of the 
+	 *  HttpServletRequest and places them in a new AwRequest. Also places the subdomain from the request URL into the AwRequest.
+	 *  Validation of the data is performed within a controller.
+	 */
+	public AwRequest createFrom(HttpServletRequest request) {
+		String subdomain = StringUtils.retrieveSubdomainFromUrlString(request.getRequestURL().toString());
+		
+		String userName = request.getParameter("u");
+		String requestType = request.getParameter("t");
+		String phoneVersion = request.getParameter("phv");
+		String protocolVersion = request.getParameter("prv");
+		String jsonData = request.getParameter("d");
+		
+		UserImpl user = new UserImpl();
+		user.setUserName(userName);
+		
+		AwRequestImpl awRequest = new AwRequestImpl();
+		awRequest.setUser(user);
+		awRequest.getPayload().put("subdomain", subdomain);
+		awRequest.getPayload().put("requestType", requestType);
+		awRequest.getPayload().put("phoneVersion", phoneVersion);
+		awRequest.getPayload().put("protocolVersion", protocolVersion);
+		awRequest.getPayload().put("jsonData", jsonData);
+		
+		return awRequest;
+	}
+}
+
