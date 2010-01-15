19615c159349c9a4ad60809866fc4f6130b40df5
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-15 01:33:26 +0000

    added comments

--- src/edu/ucla/cens/awserver/validator/AwRequestAnnotator.java
+++ src/edu/ucla/cens/awserver/validator/AwRequestAnnotator.java
@@ -10,7 +10,8 @@ import edu.ucla.cens.awserver.datatransfer.AwRequest;
 public interface AwRequestAnnotator {
 	
 	/**
-	 * Annotates the provided AwRequest with details of the failed validation. 
+	 * Annotates the AwRequest with the provided message. Implementations of this interface are free to annotate the 
+	 * request in ways specific to a feature or usage scenario (e.g., failed validation, creation of custom error messages, etc). 
 	 */
 	public void annotate(AwRequest request, String message);
 	
