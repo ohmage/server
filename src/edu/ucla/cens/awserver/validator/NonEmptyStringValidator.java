200c2a7ffa310e0231f6a58e5e38d4b9a3f8455f
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-15 01:36:52 +0000

    now explicitly to be used as a validator for internally set values that are required by components in the Controller layer, but set within the HTTP (servlet, filter) layer

--- src/edu/ucla/cens/awserver/validator/NonEmptyStringValidator.java
+++ src/edu/ucla/cens/awserver/validator/NonEmptyStringValidator.java
@@ -4,13 +4,16 @@ import edu.ucla.cens.awserver.datatransfer.AwRequest;
 import edu.ucla.cens.awserver.util.StringUtils;
 
 /**
- * Validates a string item from the payload Map from an AwRequest. 
+ * Validator for *internally set* AwRequest attributes. This validator should only be used to check required values that are 
+ * internally set by the HTTP layer (servlets, filters, AwRequestCreators) where it is a logical error for the value to be missing. 
+ * E.g., most controllers in AW require a subdomain attribute in the AwRequest, but the subdomain value is set by parsing the  
+ * URL string, not by a user-driven ("external") action. Put another way, it is a logical error for the subdomain to be missing.
+ * For validation of user-driven data, use a subclass of AbstractAnnotatingValidator.
  * 
  * @author selsky
- * @see AwRequest
  */
 public class NonEmptyStringValidator implements Validator {
-	private String _payloadItemName;
+	private String _awRequestAttributeName;
 
 	/**
 	 * Creates an instance of this class where the payloadItemName parameter is used as a key to retrieve the value that will be 
@@ -18,21 +21,25 @@ public class NonEmptyStringValidator implements Validator {
 	 * 
 	 * @throws IllegalArgumentException if the payloadItemName parameter is empty or null or all whitespace
 	 */
-	public NonEmptyStringValidator(String payloadItemName) {
-		if(StringUtils.isEmptyOrWhitespaceOnly(payloadItemName)) {
-			throw new IllegalArgumentException("cannot create class: empty payloadItemName parameter.");
+	public NonEmptyStringValidator(String awRequestAttributeName) {
+		if(StringUtils.isEmptyOrWhitespaceOnly(awRequestAttributeName)) {
+			throw new IllegalArgumentException("cannot create class: empty awRequestAttributeName parameter.");
 		}
 		
-		_payloadItemName = payloadItemName;
+		_awRequestAttributeName = awRequestAttributeName;
 	}
 	
 	/**
 	 * Using the key passed in on construction, retrieves a value from the payload Map from the passed in AwRequest and checks
 	 * whether it is empty.
-	 * @return false if the payload value is empty, null, or all whitespace
-	 * @return true otherwise
+     *
+	 * @return true if the value is a non-empty, non-all-whitespace string
+	 * @throws ValidatorException if the retrieved value is null, empty, or all whitespace 
 	 */
 	public boolean validate(AwRequest awRequest) {
-		return ! (StringUtils.isEmptyOrWhitespaceOnly((String) awRequest.getAttribute(_payloadItemName)));
+		if(StringUtils.isEmptyOrWhitespaceOnly((String) awRequest.getAttribute(_awRequestAttributeName))) {
+			return true;
+		}
+		throw new ValidatorException("missing " + _awRequestAttributeName + " from AwRequest - cannot continue processing");
 	}
 }
