629f78fa8e0f4248cf65632ab61d0f8f1ebc13fc
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-15 01:34:20 +0000

    log error messages if the logger is configured at the debug level

--- src/edu/ucla/cens/awserver/validator/FailedRequestAnnotator.java
+++ src/edu/ucla/cens/awserver/validator/FailedRequestAnnotator.java
@@ -1,19 +1,27 @@
 package edu.ucla.cens.awserver.validator;
 
+import org.apache.log4j.Logger;
+
 import edu.ucla.cens.awserver.datatransfer.AwRequest;
 
 /**
- * An implementation of AwRequestAnnotator for validation failures.
+ * An implementation of AwRequestAnnotator for general validation failures.
  * 
  * @author selsky
  */
 public class FailedRequestAnnotator implements AwRequestAnnotator {
-
+	private static Logger _logger = Logger.getLogger(FailedRequestAnnotator.class);
+	
 	/**
-     * Sets failed request properties on the AwRequest.
+     * On the AwRequest, sets both the failedRequest property to true and the failedRequestErrorMessage property to the passed-in
+     * message. 
 	 */
 	public void annotate(AwRequest request, String message) {
 		request.setFailedRequest(true);
 		request.setFailedRequestErrorMessage(message);
+		
+		if(_logger.isDebugEnabled()) {
+			_logger.debug(message);
+		}
 	}
 }
