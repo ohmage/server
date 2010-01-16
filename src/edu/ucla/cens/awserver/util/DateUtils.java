18dd9f30e9463202866bd23383977484a027ad2f
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-16 00:53:08 +0000

    date utilities

---
+++ src/edu/ucla/cens/awserver/util/DateUtils.java
@@ -0,0 +1,52 @@
+package edu.ucla.cens.awserver.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * A collection of static methods for working with dates.
+ * 
+ * @author selsky
+ */
+public class DateUtils {
+//	private static Logger _logger = Logger.getLogger(DateUtils.class);
+	
+	/**
+	 * Instatiation disallowed and unnecessary.
+	 */
+	private DateUtils() { }
+	
+	/**
+     * Assumes dates are of the form yyyy-MM-dd hh:mm:ss.
+     * 
+     * @throws IllegalArgumentException if the timezone is unknown
+     * @throws IllegalArgumentException if the date is null 
+	 */
+	public static String convertDateToUtc(Date date, String timezone) {
+		if(null == date) {
+			throw new IllegalArgumentException("a non-null date is required");
+		}
+		if(! isValidTimezone(timezone)) {
+			throw new IllegalArgumentException("a valid timezone is required");
+		}
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		sdf.setTimeZone(TimeZone.getTimeZone(timezone));
+		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+		
+		return sdf.format(date);
+	}
+	
+	private static boolean isValidTimezone(String tz) {
+		if(null != tz) {
+			String[] ids = TimeZone.getAvailableIDs();
+			for(String id : ids) {
+				if(id.equals(tz)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
