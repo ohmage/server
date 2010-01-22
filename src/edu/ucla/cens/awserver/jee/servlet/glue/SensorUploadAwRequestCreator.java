ee7857266158062cab4fc978af31b00a4d62c8cb
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-22 00:52:19 +0000

    now URL decodes the JSON

--- src/edu/ucla/cens/awserver/jee/servlet/glue/SensorUploadAwRequestCreator.java
+++ src/edu/ucla/cens/awserver/jee/servlet/glue/SensorUploadAwRequestCreator.java
@@ -1,5 +1,8 @@
 package edu.ucla.cens.awserver.jee.servlet.glue;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import javax.servlet.http.HttpServletRequest;
 
 import edu.ucla.cens.awserver.datatransfer.AwRequest;
@@ -34,7 +37,17 @@ public class SensorUploadAwRequestCreator implements AwRequestCreator {
 		String requestType = request.getParameter("t");
 		String phoneVersion = request.getParameter("phv");
 		String protocolVersion = request.getParameter("prv");
-		String jsonData = request.getParameter("d");
+		String jsonData = null; 
+		try {
+			
+			jsonData = URLDecoder.decode(request.getParameter("d"), "UTF-8");
+			
+		} catch(UnsupportedEncodingException uee) { // if UTF-8 is not recognized we have big problems
+			
+			throw new IllegalStateException(uee);
+		}
+		
+		// _logger.info(jsonData);
 		
 		UserImpl user = new UserImpl();
 		user.setUserName(userName);
@@ -47,7 +60,12 @@ public class SensorUploadAwRequestCreator implements AwRequestCreator {
 		awRequest.setAttribute("protocolVersion", protocolVersion);
 		awRequest.setAttribute("jsonData", jsonData);
 		
-		awRequest.setAttribute("requestUrl", request.getRequestURL().toString() + "?" +  request.getQueryString()); // output in reponse in case of error
+		String requestUrl = request.getRequestURL().toString();
+		if(null != request.getQueryString()) {
+			requestUrl += "?" + request.getQueryString(); 
+		}
+		
+		awRequest.setAttribute("requestUrl", requestUrl); // output in reponse in case of error
 		
 		return awRequest;
 	}
