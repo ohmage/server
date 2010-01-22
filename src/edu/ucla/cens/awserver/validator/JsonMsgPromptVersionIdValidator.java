fd23c7223330ef6d0ef210dd751ded8401cf448d
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-22 23:15:27 +0000

    validator for version_id in prompt response uploads

---
+++ src/edu/ucla/cens/awserver/validator/JsonMsgPromptVersionIdValidator.java
@@ -0,0 +1,53 @@
+package edu.ucla.cens.awserver.validator;
+
+import org.json.JSONObject;
+
+import edu.ucla.cens.awserver.dao.Dao;
+import edu.ucla.cens.awserver.dao.DataAccessException;
+import edu.ucla.cens.awserver.datatransfer.AwRequest;
+import edu.ucla.cens.awserver.util.JsonUtils;
+
+/**
+ * Validator for the version id from a prompt message.
+ * 
+ * @author selsky
+ */
+public class JsonMsgPromptVersionIdValidator extends AbstractDaoAnnotatingJsonObjectValidator {
+	private String _key = "version_id";
+		
+	public JsonMsgPromptVersionIdValidator(AwRequestAnnotator awRequestAnnotator, Dao dao) {
+		super(awRequestAnnotator, dao);
+	}
+	
+	/**
+	 * @return true if the value returned from the JSONObject for the key "version_id" exists and is a valid version id for the 
+	 * campaign found in the AwRequest.
+	 * @return false otherwise
+	 */
+	public boolean validate(AwRequest request, JSONObject jsonObject) {		 
+		String versionId = JsonUtils.getStringFromJson(jsonObject, _key);
+		
+		if(null == versionId) {
+			getAnnotator().annotate(request, "version_id in message is null or invalid");
+			return false;
+		}
+		
+		request.setAttribute("versionId", versionId);
+		
+		try {
+		
+			getDao().execute(request);
+			
+		} catch(DataAccessException daoe) { // unrecoverable error, just rethrow
+			
+			throw new ValidatorException(daoe);
+		}
+		
+		if(request.isFailedRequest()) {
+			getAnnotator().annotate(request, "prompt version_id " + versionId + " not found for campaign " + request.getAttribute("subdomain"));
+			return false;
+		}
+		
+		return true;
+	}
+}
