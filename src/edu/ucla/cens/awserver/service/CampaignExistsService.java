d8220e00aa1daf07100aad4bd0d982e1d0332904
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-22 00:45:18 +0000

    now extends AbstractAnnotatingDaoService

--- src/edu/ucla/cens/awserver/service/CampaignExistsService.java
+++ src/edu/ucla/cens/awserver/service/CampaignExistsService.java
@@ -6,20 +6,31 @@ import java.util.List;
 import edu.ucla.cens.awserver.dao.Dao;
 import edu.ucla.cens.awserver.dao.DataAccessException;
 import edu.ucla.cens.awserver.datatransfer.AwRequest;
+import edu.ucla.cens.awserver.util.StringUtils;
+import edu.ucla.cens.awserver.validator.AwRequestAnnotator;
 
 /**
  * Dispatches to a DAO to perform campaign existence checks.
  * 
+ * TODO - this class could be used in any situation where all that needs to be checked is the existence of a single row in the DB
+ * 
  * @author selsky
  */
-public class CampaignExistsService extends AbstractDaoService {
+public class CampaignExistsService extends AbstractAnnotatingDaoService {
 //	private static Logger _logger = Logger.getLogger(CampaignExistsService.class);
+	private String _errorMessage;
 	
 	/**
 	 * Creates an instances of this class using the supplied DAO as the method of data access.
+	 * 
+	 * @throws IllegalArgumentException if the error message is empty, all whitespace, or null
 	 */
-	public CampaignExistsService(Dao dao) {
-		super(dao);
+	public CampaignExistsService(Dao dao, AwRequestAnnotator awRequestAnnotator, String errorMessage) {
+		super(dao, awRequestAnnotator);
+		if(StringUtils.isEmptyOrWhitespaceOnly(errorMessage)) {
+			throw new IllegalArgumentException("error message cannot be empty or null");
+		}
+		_errorMessage = errorMessage;
 	}
 	
 	/**
@@ -32,9 +43,9 @@ public class CampaignExistsService extends AbstractDaoService {
 			getDao().execute(request);
 			List<?> results = (List<?>) request.getAttribute("results");
 			
-			if(null != results &&  ! results.isEmpty()) { 
+			if(null == results || results.isEmpty()) { 
 				
-				request.setAttribute("campaignExistsForSubdomain", "true");
+				getAnnotator().annotate(request, _errorMessage);
 				
 			}
 			
