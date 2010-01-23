1d32c8bfff13cc9dff1cff3a27687b6176e7408d
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-23 00:21:53 +0000

    prompt message group_id validation

---
+++ src/edu/ucla/cens/awserver/validator/JsonMsgPromptGroupIdValidator.java
@@ -0,0 +1,55 @@
+package edu.ucla.cens.awserver.validator;
+
+import org.json.JSONObject;
+
+import edu.ucla.cens.awserver.dao.Dao;
+import edu.ucla.cens.awserver.dao.DataAccessException;
+import edu.ucla.cens.awserver.datatransfer.AwRequest;
+import edu.ucla.cens.awserver.util.JsonUtils;
+
+/**
+ * Validator for the group id from a prompt message.
+ * 
+ * TODO this class is nearly identical to JsonMsgPromptVersionIdValidator
+ * 
+ * @author selsky
+ */
+public class JsonMsgPromptGroupIdValidator extends AbstractDaoAnnotatingJsonObjectValidator {
+	private String _key = "group_id";
+		
+	public JsonMsgPromptGroupIdValidator(AwRequestAnnotator awRequestAnnotator, Dao dao) {
+		super(awRequestAnnotator, dao);
+	}
+	
+	/**
+	 * @return true if the value returned from the JSONObject for the key "group_id" exists and is a valid group id for the 
+	 * campaign found in the AwRequest.
+	 * @return false otherwise
+	 */
+	public boolean validate(AwRequest request, JSONObject jsonObject) {		 
+		String groupId = JsonUtils.getStringFromJson(jsonObject, _key);
+		
+		if(null == groupId) {
+			getAnnotator().annotate(request, "group_id in message is null or invalid");
+			return false;
+		}
+		
+		request.setAttribute("groupId", groupId);
+		
+		try {
+		
+			getDao().execute(request);
+			
+		} catch(DataAccessException daoe) { // unrecoverable error, just rethrow
+			
+			throw new ValidatorException(daoe);
+		}
+		
+		if(request.isFailedRequest()) {
+			getAnnotator().annotate(request, "prompt group_id " + groupId + " not found for campaign " + request.getAttribute("subdomain"));
+			return false;
+		}
+		
+		return true;
+	}
+}
