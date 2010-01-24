bbc77d9ec571a380341c6d0679701c82dd976df6
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-24 22:03:07 +0000

    DAO for retrieving prompt type information for a group of prompt responses

---
+++ src/edu/ucla/cens/awserver/dao/PromptResponseGroupDao.java
@@ -0,0 +1,128 @@
+package edu.ucla.cens.awserver.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+
+import edu.ucla.cens.awserver.datatransfer.AwRequest;
+import edu.ucla.cens.awserver.domain.PromptType;
+import edu.ucla.cens.awserver.util.StringUtils;
+
+/**
+ * DAO for returning prompt type validation restrictions for an uploaded response group. The SQL that runs here assumes that the 
+ * version_id and group_id have already been validated against the campaign. Note that the prompt_id passed as part of the upload
+ * message is the prompt_id that is shared with the phone configuration, not that actual prompt id in the db. 
+ * 
+ * @author selsky
+ */
+public class PromptResponseGroupDao extends AbstractDao {
+	private static Logger _logger = Logger.getLogger(PromptGroupIdDao.class);
+	
+	private final String _countSql = "select count(*) from prompt " +
+									 " where campaign_prompt_group_id = ?" +
+									 " and campaign_prompt_version_id = ?";
+	
+	// This query has a custom substitution (the {$..} strings) because using variables in both the WHERE and IN clauses
+	// causes the PreparedStatement implementation in the MySQL JDBC connector to throw exceptions (BadGrammarException) 
+	// after the bind variables are set.
+	private String _selectSql = "select prompt_type.type, prompt_type.restriction from prompt, prompt_type" +
+			                    " where prompt.campaign_prompt_group_id = {$campaignPromptGroupId}" +
+	                            " and prompt.campaign_prompt_version_id = {$campaignPromptVersionId}" +
+	                            " and prompt.prompt_type_id = prompt_type.id" + 
+	                            " and prompt.prompt_config_id in"; // the IN clause is dynamic depending on the prompts 
+                                                                   // in the response
+
+	/**
+	 * Creates an instance of this class that will use the supplied DataSource for data retrieval.
+	 */
+	public PromptResponseGroupDao(DataSource dataSource) {
+		super(dataSource);
+	}
+	
+	/**
+	 * Retrieves the prompt type and restrictions on the type based on the promptIdArray attribute found in the AwRequest.
+	 */
+	public void execute(AwRequest request) {
+		_logger.info("looking up prompt restrictions for prompts " + 
+			StringUtils.intArrayToString((int[]) request.getAttribute("promptIdArray")) + " for campaign " + 
+			request.getAttribute("subdomain")
+		);
+		
+		// TODO -- this new call only needs to be done once, not for every execute() method invocation
+		JdbcTemplate template = new JdbcTemplate(getDataSource());
+		
+		try {
+			int promptGroupId = (Integer) request.getAttribute("campaignPromptGroupId");
+			int promptVersionId = (Integer) request.getAttribute("campaignPromptVersionId");
+			
+			_logger.info("pgid=" + promptGroupId + " pvid=" + promptVersionId);
+			
+			// dynamically generate the SQL in clause based on the idArray
+			final int[] idArray = (int[]) request.getAttribute("promptIdArray");
+			int size = idArray.length;
+			
+			// First, check whether the number of entries in the array represents the correct number of prompts for the group
+			int numberOfPromptsInGroup = template.queryForInt(_countSql, new Object[]{promptGroupId, promptVersionId});
+			
+			if(size != numberOfPromptsInGroup) {
+				_logger.info("incorrect number of prompts for group. prompts received = " + size + ". prompts expected = " 
+					+ numberOfPromptsInGroup);
+				request.setFailedRequest(true);
+				return;
+			}
+			
+			// If the number of prompt responses is correct for the group, grab the prompt restrictions
+			StringBuilder builder = new StringBuilder(_selectSql + " (");
+			for(int i = 0; i < size; i++) {
+				builder.append("?");
+				if(i < size - 1) {
+					builder.append(",");
+				}
+			}
+			builder.append(")");
+			
+			final String sql = builder.toString()
+				.replace("{$campaignPromptGroupId}", String.valueOf(promptGroupId))
+				.replace("{$campaignPromptVersionId}", String.valueOf(promptVersionId));
+						
+			_logger.info(sql);
+			
+			List<?> list = template.query( 
+				new PreparedStatementCreator() {
+					public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+						PreparedStatement ps = connection.prepareStatement(sql);
+						for(int j = 0; j < idArray.length; j++) {
+							ps.setInt(j + 1, idArray[j]);
+						}
+						return ps;
+					}
+				},
+				new RowMapper() {
+					public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+						PromptType pt = new PromptType();
+						pt.setType(rs.getString(1));
+						pt.setRestriction(rs.getString(2));
+						return pt;
+					}
+				}
+			);
+					
+			request.setAttribute("promptRestrictions", list);
+			
+		}  catch (org.springframework.dao.DataAccessException dae) {
+			
+			throw new DataAccessException(dae); // Wrap the Spring exception and re-throw in order to avoid outside dependencies
+			                                    // on the Spring Exception (in case Spring JDBC is replaced with another lib in 
+			                                    // the future).
+		}
+	}
+}
