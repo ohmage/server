6cc14298631bce12bc6cba2d07251887165b0287
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-25 18:03:40 +0000

    added debugging messages

--- src/edu/ucla/cens/awserver/validator/json/JsonMsgPromptResponsesValidator.java
+++ src/edu/ucla/cens/awserver/validator/json/JsonMsgPromptResponsesValidator.java
@@ -85,62 +85,56 @@ public class JsonMsgPromptResponsesValidator extends AbstractDaoAnnotatingJsonOb
 			return false;
 		}
 		
-		List<?> promtpTypeList = (List<?>) request.getAttribute("promptRestrictions");
+		List<?> promptTypeList = (List<?>) request.getAttribute("promptRestrictions");
 		
-		if(null == promtpTypeList || promtpTypeList.isEmpty()) {
+		if(null == promptTypeList || promptTypeList.isEmpty()) {
 			getAnnotator().annotate(request, "prompt type restrictions not found");
 			return false;
 		}
 		
-		if(promtpTypeList.size() != jsonArrayLength) {
+		if(promptTypeList.size() != jsonArrayLength) {
 			getAnnotator().annotate(request, "incorrect number of prompt type restrictions found");
 			return false;
 		}
 		
-		for(Object o : promtpTypeList) {
-			_logger.info(o);
-		}
+//		if(_logger.isDebugEnabled()) {
+//			StringBuilder builder = new StringBuilder();
+//			
+//			for(Object o : promptTypeList) {
+//				builder.append(o);
+//				builder.append("\n");
+//			}
+//			
+//			_logger.debug("found the following prompts to validate against: " + builder.toString());
+//		}
 		
-		// ok, now the prompt response can be validated
-		// check the prompt type. if necessary, parse the restriction JSON
-
-//			INSERT INTO prompt_type (type, restriction) VALUES
-//			  ("time_military", NULL),
-//			  ("array_boolean", "3"),
-//			  ("map", '{0:"10",1:"20",2:"30",3:"40",4:"50",5:"60+"}'),
-//			  ("map", '{0:"<4",1:"5",2:"6",3:"7",4:"8",5:">8"}'),
-//			  ("map", '{0:"very bad",1:,2:,3:"very good"}'),
-//			  ("map", '{0:"not at all",1:,2:"slightly",3:,4:"moderately",5:,6:"extremely"}'),
-//			  ("map", '{0:"never",1:"almost never",2:"sometimes",3:"fairly often",4:"very often"}'),
-//			  ("map", '{0:"none",1:"light",2:"moderate",3:"vigorous"}'),
-//			  ("map", '{0:"1",1:"2",2:"3",3:"4",4:"5",5:"6",6:"7",7:"8",8:"9",9:"10+"}'),
-//			  ("map", '{1:"Yes",0:"No"}'),
-//			  ("array_boolean", "6"),
-//			  ("null", NULL),
-//			  ("map", '{0:"10",1:"20",2:"30",3:"40",4:"50",5:"60+",6:"N/A"}');
-        
 		// Now validate the contents of each prompt response
 		
 		for(int i = 0; i < jsonArrayLength; i++) {
 			
 			JSONObject promptResponse = JsonUtils.getJsonObjectFromJsonArray(jsonArray, i);
 			
-			PromptType promptType = getPromptTypeForPromptConfigId(promtpTypeList, 
+			PromptType promptType = getPromptTypeForPromptConfigId(promptTypeList, 
 					JsonUtils.getIntegerFromJson(promptResponse, "prompt_id"));
 			
 			String response = JsonUtils.getStringFromJson(promptResponse, "response");
 			
 			PromptResponseValidator validator = PromptResponseValidatorFactory.make(promptType);
 			
+			if(_logger.isDebugEnabled()) {
+				_logger.info("validating response value " + response + " using " + promptType);
+			}
+			
 			if(! validator.validate(response)) {
-				
 				getAnnotator().annotate(request, "invalid prompt response");
 				return false;
-				
 			}
 		}
 		
-		_logger.info("successfully validated prompt responses");
+		if(_logger.isDebugEnabled()) {
+			_logger.debug("successfully validated prompt responses");
+		}
+		
 		return true;
 	}
 	
