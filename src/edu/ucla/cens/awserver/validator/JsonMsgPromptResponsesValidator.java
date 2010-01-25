fcf09f60945feb43830b0764aa382d3ce4a83fee
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-25 02:21:14 +0000

    added dispatch to specific prompt validators based on the prompt type

--- src/edu/ucla/cens/awserver/validator/JsonMsgPromptResponsesValidator.java
+++ src/edu/ucla/cens/awserver/validator/JsonMsgPromptResponsesValidator.java
@@ -9,7 +9,10 @@ import org.json.JSONObject;
 import edu.ucla.cens.awserver.dao.Dao;
 import edu.ucla.cens.awserver.dao.DataAccessException;
 import edu.ucla.cens.awserver.datatransfer.AwRequest;
+import edu.ucla.cens.awserver.domain.PromptType;
 import edu.ucla.cens.awserver.util.JsonUtils;
+import edu.ucla.cens.awserver.validator.prompt.PromptResponseValidator;
+import edu.ucla.cens.awserver.validator.prompt.PromptResponseValidatorFactory;
 
 /**
  * Validator for the responses from a prompt message.
@@ -33,17 +36,17 @@ public class JsonMsgPromptResponsesValidator extends AbstractDaoAnnotatingJsonOb
 	 * @return false otherwise
 	 */
 	public boolean validate(AwRequest request, JSONObject jsonObject) {		 
-		JSONArray array = JsonUtils.getJsonArrayFromJson(jsonObject, _key);
+		JSONArray jsonArray = JsonUtils.getJsonArrayFromJson(jsonObject, _key);
 		
 		// Each element in the array must contain a prompt_id and a response element
 		// The response element is allowed to be null signifying "no response"
-		int length = array.length();
-		int[] idArray = new int[length];
+		int jsonArrayLength = jsonArray.length();
+		int[] idArray = new int[jsonArrayLength];
 		
 		// first, do some preliminary validation against the data
-		for(int i = 0; i < length; i++) {
+		for(int i = 0; i < jsonArrayLength; i++) {
 		
-			JSONObject object = JsonUtils.getJsonObjectFromJsonArray(array, i);
+			JSONObject object = JsonUtils.getJsonObjectFromJsonArray(jsonArray, i);
 			
 			if(null == object) {
 				getAnnotator().annotate(request, "null object for prompt responses array at responses index " + i);
@@ -56,16 +59,6 @@ public class JsonMsgPromptResponsesValidator extends AbstractDaoAnnotatingJsonOb
 				getAnnotator().annotate(request, "missing or invalid prompt_id for prompt responses array at responses index " + i);
 				return false;
 			}
-			
-//			// validate that the prompt response is valid JSON
-//			
-//			JSONObject responseObject = JsonUtils.getObjectFromJson(object, "response");
-//			
-//			if(null == responseObject) {
-//				getAnnotator().annotate(request, "response element is missing or invalid JSON for prompt responses array at " +
-//					"responses index " + i);
-//				return false;
-//			}
 			
 			idArray[i] = id;
 		}
@@ -80,18 +73,34 @@ public class JsonMsgPromptResponsesValidator extends AbstractDaoAnnotatingJsonOb
 			
 			getDao().execute(request);
 
-			if(request.isFailedRequest()) {
-				getAnnotator().annotate(request, "invalid number of prompts for prompt group");
-				return false;
-			}
+		} catch(DataAccessException daoe) { // unrecoverable error, just rethrow
 			
-			List<?> list = (List<?>) request.getAttribute("promptRestrictions");
-			for(Object o : list) {
-				_logger.info(o);
-			}
-			
-			// ok, now the prompt response can be validated
-			// check the prompt type. if necessary, parse the restriction JSON
+			throw new ValidatorException(daoe);
+		}
+		
+		if(request.isFailedRequest()) {
+			getAnnotator().annotate(request, "invalid number of prompts for prompt group");
+			return false;
+		}
+		
+		List<?> promtpTypeList = (List<?>) request.getAttribute("promptRestrictions");
+		
+		if(null == promtpTypeList || promtpTypeList.isEmpty()) {
+			getAnnotator().annotate(request, "prompt type restrictions not found");
+			return false;
+		}
+		
+		if(promtpTypeList.size() != jsonArrayLength) {
+			getAnnotator().annotate(request, "incorrect number of prompt type restrictions found");
+			return false;
+		}
+		
+		for(Object o : promtpTypeList) {
+			_logger.info(o);
+		}
+		
+		// ok, now the prompt response can be validated
+		// check the prompt type. if necessary, parse the restriction JSON
 
 //			INSERT INTO prompt_type (type, restriction) VALUES
 //			  ("time_military", NULL),
@@ -107,14 +116,44 @@ public class JsonMsgPromptResponsesValidator extends AbstractDaoAnnotatingJsonOb
 //			  ("array_boolean", "6"),
 //			  ("null", NULL),
 //			  ("map", '{0:"10",1:"20",2:"30",3:"40",4:"50",5:"60+",6:"N/A"}');
+        
+		// Now validate the contents of each prompt response
+		
+		for(int i = 0; i < jsonArrayLength; i++) {
 			
+			JSONObject promptResponse = JsonUtils.getJsonObjectFromJsonArray(jsonArray, i);
 			
+			PromptType promptType = getPromptTypeForPromptConfigId(promtpTypeList, 
+					JsonUtils.getIntegerFromJson(promptResponse, "prompt_id"));
 			
-		} catch(DataAccessException daoe) { // unrecoverable error, just rethrow
+			String response = JsonUtils.getStringFromJson(promptResponse, "response");
 			
-			throw new ValidatorException(daoe);
+			PromptResponseValidator validator = PromptResponseValidatorFactory.make(promptType);
+			
+			if(! validator.validate(response)) {
+				
+				getAnnotator().annotate(request, "invalid prompt response");
+				return false;
+				
+			}
 		}
 		
+		_logger.info("successfully validated prompt responses");
 		return true;
+	}
+	
+	private PromptType getPromptTypeForPromptConfigId(List<?> list, int promptConfigId) {
+
+		for(Object object : list) {
+			
+			PromptType pt = (PromptType) object;
+			
+			if(pt.getPromptConfigId() == promptConfigId) {
+				return pt;
+			}
+		}
+		
+		// if this code runs, it means previous validation and db querying has logical errors
+		throw new IllegalArgumentException("no prompt type exists for prompt config id " + promptConfigId);
 	}
 }
