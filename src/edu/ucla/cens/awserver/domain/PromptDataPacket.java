ca5eedd96978903695bcf2e361425186ef037cc8
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-26 21:17:50 +0000

    internal representation of a prompt message: contains metadata and each prompt response for a particular group

---
+++ src/edu/ucla/cens/awserver/domain/PromptDataPacket.java
@@ -0,0 +1,58 @@
+package edu.ucla.cens.awserver.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Data packet implementation for the storage of a single prompt response.
+ * 
+ * @author selsky
+ */
+public class PromptDataPacket extends MetadataDataPacket {
+	
+	private List<DataPacket> _responses;
+	
+	public List<DataPacket> getResponses() {
+		return _responses;
+	}
+
+	public void setResponses(List<DataPacket> responses) {
+		_responses = responses;
+	}
+	
+	@Override
+	public String toString() {
+		return "PromptDataPacket [_responses=" + Arrays.toString(_responses.toArray()) + ", toString()="
+				+ super.toString() + "]";
+	}
+
+	public class PromptResponseDataPacket implements DataPacket {
+		private int _promptConfigId; // the prompt_id sent by the phone (not the primary key to the db table)
+		private String _response;    // a JSON Object converted to a string
+		
+		public PromptResponseDataPacket() { }
+		
+		public int getPromptConfigId() {
+			return _promptConfigId;
+		}
+		
+		public void setPromptConfigId(int promptConfigId) {
+			_promptConfigId = promptConfigId;
+		}
+		
+		public String getResponse() {
+			return _response;
+		}
+		
+		public void setResponse(String response) {
+			_response = response;
+		}
+		
+		@Override
+		public String toString() {
+			return "PromptDataPacket [_promptConfigId=" + _promptConfigId
+					+ ", _response=" + _response + "]";
+		}
+	}
+	
+}
