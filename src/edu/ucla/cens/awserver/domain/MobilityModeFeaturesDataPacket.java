11fe245d39201ffcc61accb0af128d5a41376e6c
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-26 21:26:03 +0000

    added toString(), no longer unpacks the fft array

--- src/edu/ucla/cens/awserver/domain/MobilityModeFeaturesDataPacket.java
+++ src/edu/ucla/cens/awserver/domain/MobilityModeFeaturesDataPacket.java
@@ -1,5 +1,6 @@
 package edu.ucla.cens.awserver.domain;
 
+
 /**
  * Domain object representing a mobility mode_features data packet.
  * 
@@ -9,7 +10,7 @@ public class MobilityModeFeaturesDataPacket extends MobilityModeOnlyDataPacket {
 	private Double _speed;
 	private Double _variance;
 	private Double _average;
-	private Double[] _fftArray;
+	private String _fftArrayString; // this is an unpacked JSON array
 	
 	public MobilityModeFeaturesDataPacket() {
 		
@@ -18,26 +19,40 @@ public class MobilityModeFeaturesDataPacket extends MobilityModeOnlyDataPacket {
 	public Double getSpeed() {
 		return _speed;
 	}
+	
 	public void setSpeed(Double speed) {
 		_speed = speed;
 	}
+	
 	public Double getVariance() {
 		return _variance;
 	}
+	
 	public void setVariance(Double variance) {
 		_variance = variance;
 	}
+	
 	public Double getAverage() {
 		return _average;
 	}
+	
 	public void setAverage(Double average) {
 		_average = average;
 	}
-	public Double[] getFftArray() {
-		return _fftArray;
-	}
-	public void setFftArray(Double[] fftArray) {
-		_fftArray = fftArray;
+	
+	public String getFftArray() {
+		return _fftArrayString;
 	}
 	
+	public void setFftArray(String fftArrayString) {
+		_fftArrayString = fftArrayString;
+	}
+
+	@Override
+	public String toString() {
+		return "MobilityModeFeaturesDataPacket [_average=" + _average
+				+ ", _fftArrayString=" + _fftArrayString + ", _speed="
+				+ _speed + ", _variance=" + _variance + " " + super.toString() +"]";
+	}
+		
 }
