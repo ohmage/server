2c356a280c5b26ef3ce58c9ac31db72e3f4acb6c
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-26 21:24:48 +0000

    now using AbstractDataBuilder for creation of all packet metadata

--- src/edu/ucla/cens/awserver/domain/MobilityModeOnlyDataPacketBuilder.java
+++ src/edu/ucla/cens/awserver/domain/MobilityModeOnlyDataPacketBuilder.java
@@ -1,16 +1,15 @@
 package edu.ucla.cens.awserver.domain;
 
-import org.json.JSONException;
 import org.json.JSONObject;
+
+import edu.ucla.cens.awserver.util.JsonUtils;
 
 /**
  * Builder of mobility mode-only data packets from JSONObjects.
  * 
- * TODO need a base class to build all of the common fields
- * 
  * @author selsky
  */
-public class MobilityModeOnlyDataPacketBuilder implements DataPacketBuilder {
+public class MobilityModeOnlyDataPacketBuilder extends AbstractDataPacketBuilder {
 
 	public MobilityModeOnlyDataPacketBuilder() {
 		
@@ -21,47 +20,18 @@ public class MobilityModeOnlyDataPacketBuilder implements DataPacketBuilder {
 	 * 
 	 * Performs some conversions on the incoming data to get it ready for database insertion.
 	 * <ul>
-	 * <li> Converts dates to UTC using the timezone provided in the source object
-	 * <li> Converts Double.NaN to null for latitude and longitude
-	 * <li> TODO - does anything else belong in this list?
+	 * <li> Converts dates (date and time elements from the JSON message) to UTC using the timezone provided in the source object
+	 * <li> Converts Double.NaN to null for latitude and longitude.
 	 * </ul>
      *
 	 * @throws IllegalArgumentException of the source object is not an instance of JSONObject
 	 */
-	public DataPacket createDataPacketFrom(Object source) {
-		if(! (source instanceof JSONObject)) {
-			throw new IllegalArgumentException("source object is not an instance of JSONObject");
-		}
-		
-		JSONObject json = (JSONObject) source;		
+	public MetadataDataPacket createDataPacketFrom(JSONObject source) {
 		MobilityModeOnlyDataPacket packet = new MobilityModeOnlyDataPacket();
-		
-		String date = null; 
-		long time = 0;
-		String timezone = null;
-		String subtype = null;
-		JSONObject location = null;
-		double latitude = 0.0;
-		double longitude = 0.0;
-		
-		try {
-		
-			date = json.getString("date"); 
-			time = json.getLong("time");
-			timezone = json.getString("timezone");
-			subtype = json.getString("subtype");
-			location = json.getJSONObject("location");
-			latitude = json.getDouble("latitude");
-			longitude = json.getDouble("longitude");
-			
-		} catch (JSONException jsone) { // a JSONException is thrown if a value cannot be found for a key or if the value 
-			                            // is of an incorrect type
-			
-			
-			
-		}
+	
+		createCommonFields(source, packet);
+		packet.setMode(JsonUtils.getStringFromJsonObject(source, "mode"));
 		
 		return packet;
 	}
-
 }
