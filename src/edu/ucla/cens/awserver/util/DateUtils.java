8b97edeb06ccf3c6fb9c396537951d1135f0665d
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-26 21:27:23 +0000

    added convertTimeToUtc() and cleaned up convertDateToUtc()

--- src/edu/ucla/cens/awserver/util/DateUtils.java
+++ src/edu/ucla/cens/awserver/util/DateUtils.java
@@ -1,6 +1,8 @@
 package edu.ucla.cens.awserver.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -21,9 +23,9 @@ public class DateUtils {
      * Assumes dates are of the form yyyy-MM-dd hh:mm:ss.
      * 
      * @throws IllegalArgumentException if the timezone is unknown
-     * @throws IllegalArgumentException if the date is null 
+     * @throws IllegalArgumentException if the date is null or unparseable
 	 */
-	public static String convertDateToUtc(Date date, String timezone) {
+	public static String convertDateToUtc(String date, String timezone) {
 		if(null == date) {
 			throw new IllegalArgumentException("a non-null date is required");
 		}
@@ -33,10 +35,53 @@ public class DateUtils {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		sdf.setTimeZone(TimeZone.getTimeZone(timezone)); // create the date using the original timezone
-		sdf.setTimeZone(TimeZone.getTimeZone("UTC")); // this converts the date to UTC, updating the value in the process
 		
-		return sdf.format(date);
+		Date parsedDate = null;
+		
+		try {
+			
+			parsedDate = sdf.parse(date);
+			
+		} catch (ParseException pe) {
+			
+			throw new IllegalArgumentException("unparseable date " + date);
+			
+		}
+		
+		sdf.setTimeZone(TimeZone.getTimeZone("UTC")); // this converts the date to UTC, updating the value in the process
+		return sdf.format(parsedDate);
+		 
 	}
+	
+	/**
+	 *  Converts the long value in the provided timezone to a value in UTC.
+	 */
+	public static long convertTimeToUtc(Long time, String timezone) {
+		if(! isValidTimezone(timezone)) {
+			throw new IllegalArgumentException("a valid timezone is required");
+		}
+
+		Calendar providedTime = Calendar.getInstance(TimeZone.getTimeZone(timezone));
+		providedTime.setTimeInMillis(time);
+		
+//		int hour = providedTime.get(Calendar.HOUR);
+//		int minute = providedTime.get(Calendar.MINUTE);
+//		int second = providedTime.get(Calendar.SECOND);
+//		
+//		System.out.printf("Provided time  : %02d:%02d:%02d %s \n", hour, minute, second, providedTime.get(Calendar.AM_PM));
+		
+		Calendar utcTime = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		utcTime.setTimeInMillis(providedTime.getTimeInMillis());
+		
+//		hour = utcTime.get(Calendar.HOUR);
+//		minute = utcTime.get(Calendar.MINUTE);
+//		second = utcTime.get(Calendar.SECOND);
+//		
+//		System.out.printf("UTC time  : %02d:%02d:%02d %s \n", hour, minute, second, utcTime.get(Calendar.AM_PM));
+		
+		return utcTime.getTimeInMillis();
+	}
+	
 	
 	public static boolean isValidTimezone(String tz) {
 		if(null != tz) {
