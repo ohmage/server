67fd71fa0c3c2163f559c7007ad90cff6942fa8b
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-26 21:19:21 +0000

    base abstract builder for message metadata

---
+++ src/edu/ucla/cens/awserver/domain/AbstractDataPacketBuilder.java
@@ -0,0 +1,43 @@
+package edu.ucla.cens.awserver.domain;
+
+import org.json.JSONObject;
+
+import edu.ucla.cens.awserver.util.DateUtils;
+import edu.ucla.cens.awserver.util.JsonUtils;
+
+/**
+ * Abstract helper class for handling fields common to all packets.
+ * 
+ * @author selsky
+ */
+public abstract class AbstractDataPacketBuilder implements DataPacketBuilder {
+	
+	/**
+	 * Sets the values for the fields common to all packets: date, time (millis since epoch), timezone, latitude, longitude. 
+	 * Converts the date and the time to UTC values. Assumes all JSON data has been pre-validated. 
+	 */
+	public void createCommonFields(JSONObject source, MetadataDataPacket packet) {
+		String date = JsonUtils.getStringFromJsonObject(source, "date");
+		long time = JsonUtils.getLongFromJsonObject(source, "time");
+		String timezone = JsonUtils.getStringFromJsonObject(source, "timezone");
+		JSONObject location = JsonUtils.getJsonObjectFromJsonObject(source, "location");
+		Double latitude = checkForDoubleNaN(location, "latitude");
+		Double longitude = checkForDoubleNaN(location, "longitude");
+		
+		String stringUtcDate = DateUtils.convertDateToUtc(date, timezone);
+		long utcTime = DateUtils.convertTimeToUtc(time, timezone);
+		
+		packet.setUtcDate(stringUtcDate);
+		packet.setUtcTime(utcTime);
+		packet.setTimezone(timezone);
+		packet.setLatitude(latitude);
+		packet.setLongitude(longitude);
+	}
+	
+	private Double checkForDoubleNaN(JSONObject source, String key) {
+		// latitude and longtidue may be sent with the string value "Double.NaN"
+		Double value = JsonUtils.getDoubleFromJsonObject(source, key);
+		
+		return null == value? Double.NaN : value;
+	}
+}
