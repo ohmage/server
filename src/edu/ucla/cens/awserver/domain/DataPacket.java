0ea54bb93202547a7a228b4cd40eb8acae664fd2
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-26 21:16:14 +0000

    marker interface for data packets

---
+++ src/edu/ucla/cens/awserver/domain/DataPacket.java
@@ -0,0 +1,12 @@
+package edu.ucla.cens.awserver.domain;
+
+/**
+ * Marker interface for upload data packets created from JSON messages.
+ * 
+ * @author selsky
+ */
+public interface DataPacket {
+	
+	
+	
+}
