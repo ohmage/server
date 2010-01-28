f986d77ce19ae1f618fe0ccd9abb293d0976e90b
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-01-28 01:22:53 +0000

    now handles JdbcTemplate locally

--- src/edu/ucla/cens/awserver/dao/AbstractDao.java
+++ src/edu/ucla/cens/awserver/dao/AbstractDao.java
@@ -2,6 +2,8 @@ package edu.ucla.cens.awserver.dao;
 
 import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Provides base classes with access to a JDBC DataSource.
  * 
@@ -10,6 +12,7 @@ import javax.sql.DataSource;
 public abstract class AbstractDao implements Dao {
 	
 	private DataSource _dataSource;
+	private JdbcTemplate _jdbcTemplate;
     
 	public AbstractDao(DataSource dataSource) {
 		if(null == dataSource) {
@@ -17,9 +20,14 @@ public abstract class AbstractDao implements Dao {
 		}
 		
 		_dataSource = dataSource;
+		_jdbcTemplate = new JdbcTemplate(_dataSource);
 	}
 	
 	protected DataSource getDataSource() {
 		return _dataSource;
+	}
+	
+	protected JdbcTemplate getJdbcTemplate() {
+		return _jdbcTemplate;
 	}
 }
