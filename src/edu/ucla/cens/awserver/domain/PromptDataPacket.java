c670e5160a5efe21243061b69ae64034d7b73f1e
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-03-03 20:26:25 +0000

    added _groupId and fixed toString()

--- src/edu/ucla/cens/awserver/domain/PromptDataPacket.java
+++ src/edu/ucla/cens/awserver/domain/PromptDataPacket.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Data packet implementation for the storage of a single prompt response.
+ * Data packet implementation for the storage of a prompt response group.
  * 
  * @author selsky
  */
 public class PromptDataPacket extends MetadataDataPacket {
-	
+	private int _groupId; // the group_id sent by the phone, not the id on the campaign_prompt_group table
 	private List<PromptResponseDataPacket> _responses;
 	
 	public List<PromptResponseDataPacket> getResponses() {
@@ -20,9 +20,17 @@ public class PromptDataPacket extends MetadataDataPacket {
 		_responses = responses;
 	}
 	
+	public int getGroupId() {
+		return _groupId;
+	}
+	
+	public void setGroupId(int groupId) {
+		_groupId = groupId;
+	}
+	
 	@Override
 	public String toString() {
-		return "PromptDataPacket [_responses=" + Arrays.toString(_responses.toArray()) + ", toString()="
+		return "PromptDataPacket [_groupId=" + _groupId + ",_responses=" + (null != _responses ? Arrays.toString(_responses.toArray()) : "null") + ", toString()="
 				+ super.toString() + "]";
 	}
 
@@ -50,7 +58,7 @@ public class PromptDataPacket extends MetadataDataPacket {
 		
 		@Override
 		public String toString() {
-			return "PromptDataPacket [_promptConfigId=" + _promptConfigId
+			return "PromptResponseDataPacket [_promptConfigId=" + _promptConfigId
 					+ ", _response=" + _response + "]";
 		}
 	}
