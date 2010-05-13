76d723e507f711ea42afe6c344f17865f50d94ef
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-05-13 19:12:45 +0000

    comments

--- src/edu/ucla/cens/awserver/service/Service.java
+++ src/edu/ucla/cens/awserver/service/Service.java
@@ -3,7 +3,7 @@ package edu.ucla.cens.awserver.service;
 import edu.ucla.cens.awserver.request.AwRequest;
 
 /**
- * A service performs a discrete piece of application logic.
+ * A service performs a discrete piece of application logic as part of an AwRequest processing flow.
  * 
  * @author selsky
  */
