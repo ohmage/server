5cbd34b6e18f837c3444a688029334b1aee60ad2
Joshua Selsky <selsky@cens.ucla.edu>
2011-05-11 16:11:07 -0700

    fixed typos in preferences install section: renamed 'mobility' to 'mobilize'

--- deploy/osx-completeRebuildWithBackup.sh
+++ deploy/osx-completeRebuildWithBackup.sh
@@ -91,8 +91,8 @@ else
 	/opt/aw/thirdparty/mysql-5.1.40-osx10.5-x86_64/bin/mysql --user=andwellness --password=\&\!sickly -h localhost -P 3306 -S /opt/aw/dbs/logs/dbsd.sock andwellness </opt/aw/dbs/conf/survey_response_privacy_states.sql
 fi
 
-if [$2 == mobility]; then
-	/opt/aw/thirdparty/mysql-5.1.40-osx10.5-x86_64/bin/mysql --user=andwellness --password=\&\!sickly -h localhost -P 3306 -S /opt/aw/dbs/logs/dbsd.sock andwellness </opt/aw/dbs/conf/mobility_preferences.sql
+if [$2 == mobilize]; then
+	/opt/aw/thirdparty/mysql-5.1.40-osx10.5-x86_64/bin/mysql --user=andwellness --password=\&\!sickly -h localhost -P 3306 -S /opt/aw/dbs/logs/dbsd.sock andwellness </opt/aw/dbs/conf/mobilize_preferences.sql
 elif [$2 == chipts]; then
         /opt/aw/thirdparty/mysql-5.1.40-osx10.5-x86_64/bin/mysql --user=andwellness --password=\&\!sickly -h localhost -P 3306 -S /opt/aw/dbs/logs/dbsd.sock andwellness </opt/aw/dbs/conf/chipts_preferences.sql
 else
