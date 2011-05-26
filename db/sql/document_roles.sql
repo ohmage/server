688b83b23c5bad43c8530c6c632462e03afc6ae7
shlurbee <vhajdik@ucla.edu>
2011-05-26 12:41:09 -0700

    fix typo

--- db/sql/document_roles.sql
+++ db/sql/document_roles.sql
@@ -1,2 +1,2 @@
 -- Insert the default roles for a document.
-INSERT INTO user_class_role(role) VALUES ('reader'), ('writer'), ('owner');
+INSERT INTO document_role(role) VALUES ('reader'), ('writer'), ('owner');
