c3f55ba3a2bff1ffdf585d9b0e258bec24ed9bc9
Joshua Selsky <selsky@cens.ucla.edu>
2011-06-21 11:41:58 -0700

    added missing survey.privacy_state column and removed duplicate timestamp column

--- src/org/ohmage/jee/servlet/writer/SurveyResponseReadCsvOutputBuilder.java
+++ src/org/ohmage/jee/servlet/writer/SurveyResponseReadCsvOutputBuilder.java
@@ -53,7 +53,6 @@ public class SurveyResponseReadCsvOutputBuilder  {
 		
 		_logger.info("Generating multi-result CSV output");
 		
-		
 		Map<String, Map<String, PromptProperty>> choiceGlossaryMap = new HashMap<String, Map<String, PromptProperty>>();
 		Map<String, PromptResponseMetadata> promptResponseMetadataMap = new HashMap<String, PromptResponseMetadata>();
 		
@@ -132,7 +131,7 @@ public class SurveyResponseReadCsvOutputBuilder  {
 		
 		if(sortedColumnMapKeySet.contains("urn:ohmage:context:timestamp")) {
 			canonicalColumnList.add("urn:ohmage:context:timestamp");
-			sortedColumnMapKeySet.remove("urn:ohmage.context.timestamp");
+			sortedColumnMapKeySet.remove("urn:ohmage:context:timestamp");
 		}
 		
 		if(sortedColumnMapKeySet.contains("urn:ohmage:context:utc_timestamp")) {
@@ -340,6 +339,9 @@ public class SurveyResponseReadCsvOutputBuilder  {
 				else if("urn:ohmage:survey:description".equals(canonicalColumnId)) {
 					builder.append(cleanAndQuoteString(result.getSurveyDescription())).append(",");
 				}
+				else if("urn:ohmage:survey:privacy_state".equals(canonicalColumnId)) {
+					builder.append(result.getPrivacyState()).append(",");
+				}
 				else if(canonicalColumnId.contains("prompt:id")) {
 
 					Object value = result.getPromptResponseMap().get(canonicalColumnId.substring("urn:ohmage:prompt:id:".length()));
@@ -355,7 +357,6 @@ public class SurveyResponseReadCsvOutputBuilder  {
 					else if (value instanceof String) { // clean up text for easier input into spreadsheets 
 						String string = (String) value;
 						
-						// check to see if its a text prompt type, a survey title, or a survey description
 						if("text".equals(promptResponseMetadataMap.get(canonicalColumnId.substring("urn:ohmage:prompt:id:".length())).getPromptType())) {
 							builder.append(cleanAndQuoteString(string));
 						} else {
