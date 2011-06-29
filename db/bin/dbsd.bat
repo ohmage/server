1180c1d896f827e40b62db2e1590e8c0e4dacc48
Joshua Selsky <selsky@cens.ucla.edu>
2011-06-29 14:16:44 -0700

    as and dbs directories for scripts and config are now stored in ~/.component-repository/thirdparty on dev; the deploy scripts are stored in .component-repository/deploy; the doc directory was obsolete; old andwellness SQL for 1.3 and 2.1 was also obsolete. cleaning housegit status

--- db/bin/dbsd.bat
+++
@@ -1,2 +0,0 @@
-cd C:\opt\aw\thirdparty\mysql-5.1.46-winx64
-bin\mysqld.exe --defaults-file=C:\opt\aw\dbs\conf\my_win.cnf
--- db/sql/andwellness_2.1_schema/campaigns.sql
+++
@@ -1,4 +0,0 @@
-# Sample campaigns
-# Campaigns must exist in the system before configurations can be loaded 
-
-INSERT INTO campaign (name, label) VALUES ('NIH', 'NIH'), ('CHIPTS', 'CHIPTS');
