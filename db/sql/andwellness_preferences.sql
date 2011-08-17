7b1864598d298b8950f7650b2f5da0514ee8501e
John Jenkins <jojenki@cens.ucla.edu>
2011-08-17 15:10:17 -0700

    Updated the SQL scripts to include the new preferences for image upload and removed the unused application_name and application_version properties.

--- db/sql/andwellness_preferences.sql
+++ db/sql/andwellness_preferences.sql
@@ -1,2 +1,2 @@
 -- Adds the distribution specific preferences to the preferences table.
-INSERT INTO preference(p_key, p_value) VALUES ('default_can_create_privilege', 'false'), ('default_survey_response_sharing_state', 'private'), ('application_name', 'Ohmage');
+INSERT INTO preference(p_key, p_value) VALUES ('default_can_create_privilege', 'false'), ('default_survey_response_sharing_state', 'private');
