1e97727a3e9203284798c8d1b2ffef7d2b167109
Joshua Selsky <selsky@cens.ucla.edu>
2011-10-06 16:50:01 -0700

    fixed broken privacy state validation

--- src/org/ohmage/request/survey/SurveyResponseUpdateRequest.java
+++ src/org/ohmage/request/survey/SurveyResponseUpdateRequest.java
@@ -107,9 +107,9 @@ public class SurveyResponseUpdateRequest extends UserRequest {
 				else {
 					tPrivacyState = SurveyResponseValidators.validatePrivacyState(this, privacyStates[0]);
 					
-					if(! SurveyResponsePrivacyStateCache.instance().getKeys().contains(tPrivacyState)) {
-						setFailed(ErrorCodes.SURVEY_INVALID_PRIVACY_STATE, "Found unknown privacy_state: " + tPrivacyState);
-						throw new ValidationException("Found unknown privacy_state: " + tPrivacyState);
+					if(tPrivacyState == null) {
+						setFailed(ErrorCodes.SURVEY_INVALID_PRIVACY_STATE, "Privacy state is missing.");
+						throw new ValidationException("Privacy state is missing.");
 					}
 				}
 			}
