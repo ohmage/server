4d8083576830ac2424481709e8bea7e6e8e8d10b
John Jenkins <jojenki@cens.ucla.edu>
2012-02-10 09:36:15 -0800

    Updated the SQL build files to include the new preferences.

--- db/sql/andwellness_preferences.sql
+++ db/sql/andwellness_preferences.sql
@@ -3,4 +3,5 @@ INSERT INTO preference(p_key, p_value)
 VALUES 
     ('default_can_create_privilege', 'false'), 
     ('default_survey_response_sharing_state', 'private'),
-    ('privileged_user_in_class_can_view_others_mobility', 'true');
+    ('privileged_user_in_class_can_view_others_mobility', 'true'),
+    ('mobility_enabled', 'true');
