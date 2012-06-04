d7bf555407e3fd66621f37f818250f65d3c2329d
John Jenkins <jojenki@cens.ucla.edu>
2012-06-04 09:58:14 -0700

    Made legitimate request failures not be reported as errors.
    The logException() function in the WorkflowException class was reporting errors whenever a sub-exception was contained in this exception. Instead, it should only be reported as an error when there was no error code.

--- src/org/ohmage/exception/WorkflowException.java
+++ src/org/ohmage/exception/WorkflowException.java
@@ -179,8 +179,7 @@ public abstract class WorkflowException extends Exception {
 	 * @param logger The Logger to which this exception should be output.
 	 */
 	public void logException(final Logger logger) {
-		Throwable cause = this.getCause();
-		if(cause == null) {
+		if(annotator == null) {
 			logger.info(toString());
 		}
 		else {
