6f9bdba68732b060d9edf673dae16f56ad6b860d
Joshua Selsky <selsky@cens.ucla.edu>
2013-01-07 12:22:13 -0500

    Adding actual WhoAmI class.

---
+++ src/org/ohmage/request/auth/AuthTokenWhoAmIRequest.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright 2012 The Regents of the University of California
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.ohmage.request.auth;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.ohmage.cache.UserBin;
+import org.ohmage.exception.InvalidRequestException;
+import org.ohmage.request.UserRequest;
+
+/**
+ * <p>Request that returns a username for a logged-in user.</p>
+ * 
+ * <table border="1">
+ *   <tr>
+ *     <td>Parameter Name</td>
+ *     <td>Description</td>
+ *     <td>Required</td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@value org.ohmage.request.InputKeys#CLIENT}</td>
+ *     <td>A string describing the client that is making this request.</td>
+ *     <td>true</td>
+ *   </tr>
+ * </table>
+ * 
+ * @author John Jenkins
+ * @author Joshua Selsky
+ */
+public class AuthTokenWhoAmIRequest extends UserRequest {
+	private static final Logger LOGGER = Logger.getLogger(AuthTokenWhoAmIRequest.class);
+	private static final String KEY_USER_NAME = "username";
+	
+	/**
+	 * Creates a request for deleting the user's authentication token from the
+	 * bin.
+	 * 
+	 * @param httpRequest The HTTP request containing the parameters.
+	 * 
+	 * @throws InvalidRequestException Thrown if the parameters cannot be 
+	 * 								   parsed.
+	 * 
+	 * @throws IOException There was an error reading from the request.
+	 */
+	public AuthTokenWhoAmIRequest(HttpServletRequest httpRequest) throws IOException, InvalidRequestException {
+		super(httpRequest, null, TokenLocation.EITHER, null);
+		
+		LOGGER.info("Creating a whoami request.");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.ohmage.request.Request#service()
+	 */
+	@Override
+	public void service() {
+		LOGGER.info("Servicing the whoami request.");
+
+		if(! authenticate(AllowNewAccount.NEW_ACCOUNT_DISALLOWED)) {
+			return;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.ohmage.request.Request#respond(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	public void respond(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
+		LOGGER.info("Responding to the whoami request.");
+		
+		respond(httpRequest, httpResponse, KEY_USER_NAME, (getUser() == null) ? null : getUser().getUsername());
+	}
+}
