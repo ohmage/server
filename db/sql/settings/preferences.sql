230c0e69c1aed6cf8d18b3e4a094c680ead39c5d
John Jenkins <jojenki@cens.ucla.edu>
2013-01-18 17:06:23 -0500

    Updated the default preferences.
    The default preferences from the installation SQL script has been updated to reflect some changes within ohmage and externally.
    
    Closes #475

--- db/sql/settings/preferences.sql
+++ db/sql/settings/preferences.sql
@@ -4,11 +4,11 @@ INSERT INTO preference(p_key, p_value) VALUES
     ('image_directory', '/opt/ohmage/userdata/images'), 
     ('max_files_per_dir', '1000'), 
     ('document_depth', '5'), 
-    ('visualization_server_address', 'http://rdev.mobilizingcs.org/R/call/Mobilize/'),
+    ('visualization_server_address', 'http://opencpu.org/R/pub/Mobilize'),
     ('max_survey_response_page_size', '-1'),
     ('recaptcha_public_key', ''),
     ('recaptcha_private_key', ''),
     ('public_class_id', 'urn:class:public'),
     ('video_directory', '/opt/ohmage/userdata/videos'),
     ('audit_log_location', '/opt/ohmage/logs/audits/'),
-    ('fully_qualified_domain_name', 'http://localhost/');
+    ('fully_qualified_domain_name', 'localhost');
