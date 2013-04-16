a275961ae59080b2442301514565107a431dc0bd
John Jenkins <jojenki@cens.ucla.edu>
2013-04-16 16:23:17 -0400

    Added GETs for survey_response/read
    The survey_response/read API will now allow HTTP GETs.

--- src/org/ohmage/jee/servlet/RequestServlet.java
+++ src/org/ohmage/jee/servlet/RequestServlet.java
@@ -307,7 +307,8 @@ public class RequestServlet extends HttpServlet {
 			RequestBuilder.getInstance().getApiRegistrationRead().equals(httpRequest.getRequestURI()) ||
 			RequestBuilder.getInstance().getApiStreamRead().equals(httpRequest.getRequestURI()) ||
 			RequestBuilder.getInstance().getApiUserWhoAmI().equals(httpRequest.getRequestURI()) ||
-			RequestBuilder.getInstance().getApiOmhRead().equals(httpRequest.getRequestURI())) {
+			RequestBuilder.getInstance().getApiOmhRead().equals(httpRequest.getRequestURI()) ||
+			RequestBuilder.getInstance().getApiSurveyResponseRead().equals(httpRequest.getRequestURI())) {
 			
 			processRequest(httpRequest, httpResponse);
 		}
