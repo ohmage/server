f4d4acd8aef68b155e0953001154e7beb5339ed4
John Jenkins <jojenki@cens.ucla.edu>
2013-06-06 11:30:30 -0400

    Updated some SQL scripts.
    The update script will now safely fail and allows it to be rerun in the event that something does fail.

--- db/sql/update/2.15-2.16.sql
+++ db/sql/update/2.15-2.16.sql
@@ -1,33 +1,90 @@
--- Add the Open mHealth table for storing authentication and authorization
--- information.
-CREATE TABLE `omh_authentication` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `domain` varchar(100) NOT NULL,
-  `auth_key` varchar(100) NOT NULL,
-  `auth_value` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `omh_authentication_unique_domain_key` (`domain`,`auth_key`),
-  KEY `omh_authentication_index_domain` (`domain`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Always make sure we are using our own database.
+USE ohmage;
 
--- Add a default audio directory.
-INSERT INTO preference(p_key, p_value) VALUES
-    ('audio_directory', '/opt/ohmage/userdata/audio');
+-- Drop the procedure if it already existed.
+DROP PROCEDURE IF EXISTS upgradeFrom2Dot15To2Dot16;
 
--- Add the class creation privilege.
-ALTER TABLE user
-    ADD COLUMN `class_creation_privilege`
-    BOOLEAN NOT NULL DEFAULT FALSE
-    AFTER `campaign_creation_privilege`;
+-- Set a dummy delimiter so that our scripts may be properly formed.
+DELIMITER //
 
--- Add the user setup privilege.
-ALTER TABLE user
-    ADD COLUMN `user_setup_privilege`
-    BOOLEAN NOT NULL DEFAULT FALSE
-    AFTER `class_creation_privilege`;
+-- Create a procedure to do the things 
+CREATE PROCEDURE upgradeFrom2Dot15To2Dot16 (OUT resultCode INT)
+BEGIN
+    -- Declare a handler that will catch all SQL exceptions.
+    DECLARE exit HANDLER FOR sqlexception
+    BEGIN
+        -- Set the result code to a generic -1.
+        SET resultCode = -1;
+    END;
 
--- Add the character encoding to survey responses.
-ALTER TABLE survey_response
-    MODIFY COLUMN survey text CHARACTER SET utf8 NOT NULL;
-ALTER TABLE prompt_response
-    MODIFY COLUMN response text CHARACTER SET utf8 NOT NULL;
+	-- Add the Open mHealth table for storing authentication and authorization
+	-- information.
+	CREATE TABLE IF NOT EXISTS `omh_authentication` (
+	  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	  `domain` varchar(100) NOT NULL,
+	  `auth_key` varchar(100) NOT NULL,
+	  `auth_value` varchar(100) NOT NULL,
+	  PRIMARY KEY (`id`),
+	  UNIQUE KEY `omh_authentication_unique_domain_key` (`domain`,`auth_key`),
+	  KEY `omh_authentication_index_domain` (`domain`)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    
+    -- Add a default audio directory.
+    IF (SELECT NOT EXISTS(
+        SELECT * FROM preference
+        WHERE p_key = 'audio_directory'))
+    THEN
+		INSERT INTO preference(p_key, p_value) VALUES
+		    ('audio_directory', '/opt/ohmage/userdata/audio');
+    END IF;
+    
+    -- Add the class creation privilege.
+    IF (SELECT NOT EXISTS(
+        SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = 'ohmage'
+        AND TABLE_NAME = 'user'
+        AND COLUMN_NAME = 'class_creation_privilege'))
+    THEN
+        ALTER TABLE user
+            ADD COLUMN `class_creation_privilege`
+            BOOLEAN NOT NULL DEFAULT FALSE
+            AFTER `campaign_creation_privilege`;
+    END IF;
+
+    -- Add the user setup privilege.
+    IF (SELECT NOT EXISTS(
+        SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = 'ohmage'
+        AND TABLE_NAME = 'user'
+        AND COLUMN_NAME = 'user_setup_privilege'))
+    THEN
+		ALTER TABLE user
+		    ADD COLUMN `user_setup_privilege`
+		    BOOLEAN NOT NULL DEFAULT FALSE
+		    AFTER `class_creation_privilege`;
+    END IF;
+    
+	-- Add the character encoding to survey responses.
+	-- No check is necessary because, if these columns do not exist, there is a
+	-- serious issue. If this is already the definition of the column, there is
+	-- no harm in redefining it.
+	ALTER TABLE survey_response
+	    MODIFY COLUMN survey text CHARACTER SET utf8 NOT NULL;
+	ALTER TABLE prompt_response
+	    MODIFY COLUMN response text CHARACTER SET utf8 NOT NULL;
+
+    -- Set the result to 0.
+    SET resultCode = 0;
+END //
+
+-- Reset the delimiter to its proper value.
+DELIMITER ;
+
+-- Call the procedure.
+CALL upgradeFrom2Dot15To2Dot16(@upgradeResultCode);
+
+-- Clean up.
+DROP PROCEDURE upgradeFrom2Dot15To2Dot16;
+
+-- Echo a result message.
+SELECT @upgradeResultCode;
