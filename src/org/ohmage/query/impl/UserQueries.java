19876633e5210e7029f5848b053aaa0ebdeedbe5
jojenki <john@openmhealth.org>
2013-09-10 10:38:25 -0400

    Fixed a bug when reading user information.
    Fixed a bug when using the class or campaign lists when reading user information.
    
    References #617

--- src/org/ohmage/query/impl/UserQueries.java
+++ src/org/ohmage/query/impl/UserQueries.java
@@ -1611,7 +1611,7 @@ public class UserQueries extends Query implements IUserQueries {
 						"AND c.id = urc.campaign_id" +
 					")");
 			
-			parameters.add(campaignIds);
+			parameters.addAll(campaignIds);
 		}
 		
 		// If a collection of class IDs is present, add a WHERE clause 
@@ -1633,7 +1633,7 @@ public class UserQueries extends Query implements IUserQueries {
 						"AND c.id = uc.class_id" +
 					")");
 			
-			parameters.add(classIds);
+			parameters.addAll(classIds);
 		}
 		
 		// Always order the results by username to facilitate paging.
