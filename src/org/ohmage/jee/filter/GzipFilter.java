f9e2309cb99d9ecf8dc0a6971e81e3fcaf61bde5
Hongsuda <hongsudt@gmail.com>
2015-05-13 19:58:22 -0700

    Added some code to print out request parameter in GzipFilter. Will remove when release.

--- src/org/ohmage/jee/filter/GzipFilter.java
+++ src/org/ohmage/jee/filter/GzipFilter.java
@@ -186,6 +186,14 @@ public class GzipFilter implements Filter {
 		// Otherwise, we have successfully retrieved the parameter map, and
 		// we should save it to the request and return success.
 		else {
+			// HT: printout the result 
+			for (String key : result.keySet()){
+				String msg = "HT: key=" + key + ", value=";
+				for (String value : result.get(key))
+					msg += value + ", ";
+				LOGGER.debug(msg);
+			}
+
 			// Save the parameter map as an element of the request.
 			httpRequest.setAttribute(ATTRIBUTE_KEY_PARAMETERS, result);
 			return true;
